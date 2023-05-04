72968332849b8199385bb671fac8ebdc9a9a57c7
Ah815 <83178033+Ah815@users.noreply.github.com>
2023-05-04 20:21:02 +0500

    live

---
+++ app.js
@@ -0,0 +1,24 @@
+//var numbers = [12, 20, 50, 18.90, 39, 20, 01, 180, 75, 60];
+//var largestNumber = 0;
+//function giveLargestNumber() {
+//     for (let i = 0; i <= largestNumber; i++) {
+//          if (largestNumber < numbers[i]) {
+//               largestNumber = numbers[i];
+//          }
+//     }
+//     return largestNumber;
+//}
+//largestNumber = giveLargestNumber()
+//console.log('largestNumber->', largestNumber)
+
+//function returnValidName(arr1) {
+//     var valid = [];
+//     for (var i = 0; i < arr1.length; i++) {
+//          if (arr1[i].length >3) {
+//               valid.push(arr1[i]);
+//          }
+//     }
+//     return valid
+//}
+//var arr1 = returnValidName(["bilal", "abc", "ab", "ahmed", "faiz"])
+//console.log(arr1)
